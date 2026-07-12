3b061301a1531318683f2f759dd094e5b1b3d78c
Dantte Roberto <draaf@cin.ufpe.br>
2026-07-11 23:55:25 -0300

    feat/getSensorById: creating 2 additional functions(getSensorById and toggleAlertStatus)

--- api/src/routes.ts
+++ api/src/routes.ts
@@ -9,5 +9,7 @@ routes.get("/sensores", sensorController.getAllSensores);
 routes.post("/sensores", sensorController.createSensor);
 routes.patch("/sensores/:id", sensorController.updateSensor);
 routes.delete("/sensores/:id", sensorController.deleteSensor);
+routes.get("/sensores/:id", sensorController.getSensorById);
+routes.patch("/toggle-alert/:id", sensorController.toggleAlertStatus);
 
 export default routes;
